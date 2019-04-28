9ba2a70aec96589206a2ed29891c0e5ee556981f
mbmarcin <mbburdzy@gmail.com>
2019-04-28 10:43:55 +0200

    new

---
+++ SkuRecommendation.py
@@ -0,0 +1,16 @@
+
+"""
+import os
+import pprint
+print(os.environ['PATH'])
+pprint.pprint(os.environ['PATH'].split(';'))
+"""
+
+
+
+import pandas as pd
+
+data = {'col_1': [3, 2, 1, 0], 'col_2': ['a', 'b', 'c', 'd']}
+df = pd.DataFrame.from_dict(data)
+print(df)
+
