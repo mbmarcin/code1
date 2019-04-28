e9c3bf90d52d85667bdcfa12d17b590e606a8cc8
marcin <mbburdzy@gmail.com>
2019-04-28 14:21:43 +0200

    ok

--- SkuRecommendation.py
+++ SkuRecommendation.py
@@ -1,14 +1,14 @@
 
-"""
 import os
 import pprint
 print(os.environ['PATH'])
 pprint.pprint(os.environ['PATH'].split(';'))
-"""
+
 
 
 
 import pandas as pd
+import numpy as np
 
 data = {'col_1': [3, 2, 1, 0], 'col_2': ['a', 'b', 'c', 'd']}
 df = pd.DataFrame.from_dict(data)
