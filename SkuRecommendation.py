abb5bf0c75f5303f8cd1e94bc84b1f589c7ae817
marcin <mbburdzy@gmail.com>
2019-04-28 20:41:44 +0200

    ok

--- SkuRecommendation.py
+++ SkuRecommendation.py
@@ -10,6 +10,14 @@ pathFile = "/media/marcin/win_ssd/Users/m1/Desktop/jupyterNotebook/data.txt"
 dta = pd.read_csv(pathFile, sep=';',low_memory=False, dtype={"idCus":'str'})
 frame = dta.loc[:,['idCus','sku']].drop_duplicates()
 #usecols=['date','idCus']
+#frame['sku'].value_counts().head().to_csv('test.txt')
 
 
-print(frame.head())
+itemList = frame["sku"].unique().tolist() # upperCASE
+userCount = len(frame["idCus"].unique().tolist())
+mainFrame = pd.DataFrame(columns=('sku1','sku2','score'))
+
+print(
+
+mainFrame
+)
