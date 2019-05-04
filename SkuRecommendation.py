7591d034f4eb116c7be786c88e542001dbc37d84
marcin <mbburdzy@gmail.com>
2019-05-04 16:37:12 +0200

    v1

--- SkuRecommendation.py
+++ SkuRecommendation.py
@@ -1,20 +1,18 @@
 
-
 import pandas as pd
 
-pathFile = "/media/marcin/win_ssd/Users/m1/Desktop/jupyterNotebook/data.txt"
+pathFile = "/media/marcin/win_ssd/Users/m1/Desktop/jupyterNotebook/data.txt" # import os for path
 
+dta = pd.read_csv(pathFile, sep=';' ,low_memory=False, dtype={"idCus":'str'})
+dta = dta.loc[:,['idCus','sku','date']]
 
-dta = pd.read_csv(pathFile, sep=';',low_memory=False, dtype={"idCus":'str'})
-frame = dta.loc[:,['idCus','sku']].drop_duplicates()
-
-
+frame = dta.loc[: ,['idCus' ,'sku']].drop_duplicates()
 itemList = frame["sku"].unique().tolist() # upperCASE
 all_userCount = len(frame["idCus"].unique().tolist())
-mainFrame = pd.DataFrame(columns=('sku1','sku2','score'))
+mainFrame = pd.DataFrame(columns=('sku1' ,'sku2' ,'score'))
+
 
 row = 0
-
 for sku1 in range(len(itemList)):
     sku1ItemUser = frame[frame.sku == itemList[sku1]]["idCus"].tolist()
 
@@ -31,12 +29,13 @@ for sku1 in range(len(itemList)):
         mainFrame.loc[row] = [itemList[sku2], itemList[sku1], score]
         row += 1
 
-mainFrame.head()
-
-checkItem = 'OOMICSW7H6C'
-
-recoList = mainFrame[mainFrame.item1 == checkItem][["sku2", "score"]].sort_values("score", ascending=False)
+        print("All SKU: ", len(itemList),"SKU1: ", sku1, "SKU2: ",sku2, "all_userCount: ", all_userCount)
 
 
-print("Recommendations for item 5001\n", recoList)
+#checkItem = 'OOXXXSW7H6C'
 
+print(mainFrame.head())
+"""
+recoList = mainFrame[mainFrame.sku1 == checkItem][["sku2", "score"]].sort_values("score", ascending=False)
+print("Recommendations", recoList.head(20))
+"""
